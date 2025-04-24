e2c154399ce2c92a33a8eb5a5aa8ba0d5c3c77e5
Student-3 GameSchool <stud3.gsch124@outlook.com>
2025-04-24 20:28:01 +0700

    Add project files.

---
+++ SIRGAY/SIRGAY.cpp
@@ -0,0 +1,75 @@
+// SIRGAY.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//#include <iostream>
+//
+//
+//int main()
+//{
+//	setlocale(LC_ALL, "");
+//	srand(time(NULL));
+//
+//	
+//	
+//	for (int a = 0; a < 3;){
+//	int user, bot = 1 + rand() % 3;
+//		
+//
+//	
+//		std::cout << "Введите число\n 1 камень\n 2 ножницы\n 3 бумага\n" << std::endl;
+//		std::cin >> user;
+//		if (user == 1|| user == 2||  user == 3 ) {
+//		 
+//			if (user == 2 && bot == 1 || user == 3 && bot == 2 || user == 1 && bot == 3) {
+//				std::cout << "Ты Проиграл\n";
+//
+//			}
+//			else if (user == bot) {
+//				std::cout << "Ничья\n";
+//
+//
+//
+//
+//			}
+//			else {
+//				std::cout << "Выиграл\n";
+//				a++;
+//				std::cout << bot;
+//
+//			}
+//		}
+//		else {
+//	
+//			std::cout << "ты дурной?\n";
+//		}
+//
+//	}
+//
+ 
+#include <iostream>
+
+
+int main()
+{
+
+
+
+
+}
+
+
+
+
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
